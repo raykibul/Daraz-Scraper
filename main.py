a643e1a22ec03c66b107db2d2b9d277c0b97956e
Rakibul islam <rakibcuet16@gmail.com>
2022-07-21 01:59:33 +0600

    updated , name changed to Daraz Scraper

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import aiohttp
 import asyncio
 import requests
 from bs4 import BeautifulSoup
-import  dzParser
+import  dzScraper
 import time
 
 
@@ -11,11 +11,11 @@ import time
 async def traverse():
     count = 0
     start_time = time.time()
-    categories = dzParser.parse_categorie_links()
+    categories = dzScraper.parse_categorie_links()
     for cate in categories:
-        prodlist = await dzParser.traverse_a_category(cate)
+        prodlist = await dzScraper.traverse_a_category(cate)
         for product in prodlist:
-            qna_list = await  dzParser.parse_qna_product(product)
+            qna_list = await  dzScraper.parse_qna_product(product)
             count= count+ len(qna_list)
             print(f'Total {count} qna parsed')
 
